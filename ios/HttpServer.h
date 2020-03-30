ff6420285ce34c6b458a632528222e77af797ad4
Dylan Companjen <dylancompanjen@gmail.com>
2020-03-30 18:26:42 +0200

    Revert "Fixed wrong import"
    This reverts commit 145b388529f2a2fa612f3cf73836edc770df5554.

--- ios/HttpServer.h
+++ ios/HttpServer.h
@@ -4,7 +4,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "GCDWebUploader/GCDWebUploader.h"
+#import "GCDWebServer/GCDWebUploader.h"
 
 typedef void(^evtCallBackBlock)(NSString *event, id body);
 
